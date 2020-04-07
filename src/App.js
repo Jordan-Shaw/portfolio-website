362a56f1063ed78e5d3c05fc88cdcba29c953958
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-07 12:02:22 +0100

    Add the SVGs

--- src/App.js
+++ src/App.js
@@ -1,76 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import Introduction from './components/Introduction'
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-// import {useScrollYPosition} from 'react-use-scroll-position'
+import React, { Component } from "react";
+import Home from "./components/Home.js";
+import "./App.css";
+import NcNewsFront from "./components/projects/NcNewsFront.js";
+import NcNewsBack from "./components/projects/NcNewsBack.js";
+import NUVisionRefresh from "./components/projects/NUVisionRefresh.js";
+
 
 class App extends Component {
-  state = {
-    windowHeight: (window.innerHeight),
-    yPosition: null
-  }
 
+  
   render() {
+    const height = window.innerHeight;
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
-        <main>
-          <Introduction id="intro" smoothScroll={this.smoothScroll}/>
-          <About id="about" scrollToTop={this.scrollToTop} smoothScroll={this.smoothScroll}/>
-          <Projects id="projects" scrollToTop={this.scrollToTop} smoothScroll={this.smoothScroll} yPositionProjects={this.yPositionProjects}/>
-          <Contact id="contact" scrollToTop={this.scrollToTop} />
-        </main>
+          <Home/>
+          <NcNewsFront height={height}/>
+          <NcNewsBack height={height}/>
+          <NUVisionRefresh height={height}/>
       </div>
     );
   }
-
-
-  smoothScroll = (position) => {
-    if (position === 'about') {
-      window.scroll(
-        {
-          top: this.state.windowHeight,
-          left: 0
-        })
-    } else if (position === 'projects') {
-      window.scroll(
-        {
-          top: 2 * this.state.windowHeight,
-          left: 0
-        })
-    } else if (position === 'contact') {
-      window.scroll(
-        {
-          top: 4.5 * this.state.windowHeight,
-          left: 0
-        })
-    }
-  }
-
-
-  scrollToTop = () => {
-    window.scrollTo({
-      top: 0
-    })
-  }
-
-  determineWindowHeight = () => {
-    
-  }
-
-  yPositionProjects = (yPosition) => {
-    if (yPosition < 2 * this.state.windowHeight) {
-      return "sectionC"
-    } else if (3.5 * this.state.windowHeight > yPosition) {
-      return "sectionCfixed"
-    } else {
-      return "sectionCUnfixed"
-    }
-  }
-
 }
-
 export default App;
